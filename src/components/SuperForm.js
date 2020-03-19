163d313cd2a7cf9d1319a005960b46772633d232
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-03-19 17:29:36 -0600

    slight change

--- src/components/SuperForm.js
+++ src/components/SuperForm.js
@@ -1,44 +1,44 @@
-import React from 'react'
+// import React from 'react'
 
-const SuperForm = (props) => {
+// const SuperForm = (props) => {
 
-    // const createOptionsWithNumbers = (firstNumber, secondNumber, ) => {
-    //     let optionsArray = [];
-    //     for (let i = firstNumber; i < secondNumber; i++) {
-    //         optionsArray.push(<option value={`${i}`}>i</option>)
-    //     }
-    //     return optionsArray
-    // }
+//     // const createOptionsWithNumbers = (firstNumber, secondNumber, ) => {
+//     //     let optionsArray = [];
+//     //     for (let i = firstNumber; i < secondNumber; i++) {
+//     //         optionsArray.push(<option value={`${i}`}>i</option>)
+//     //     }
+//     //     return optionsArray
+//     // }
 
-    return(
-        // <>
-        //     <form>
-        //         <label>
-        //         Pick your favorite flavor:
+//     return(
+//         // <>
+//         //     <form>
+//         //         <label>
+//         //         Pick your favorite flavor:
                     
-        //             <select value={this.state.value} onChange={this.handleChange}>
-        //                 {createOptionsWithNumbers}
-        //             </select>
-        //             <select value={this.state.value} onChange={this.handleChange}>
-        //                 <option value="grapefruit">Grapefruit</option>
-        //                 <option value="lime">Lime</option>
-        //                 <option value="coconut">Coconut</option>
-        //                 <option value="mango">Mango</option>
-        //             </select>
-        //             <select value={this.state.value} onChange={this.handleChange}>
-        //                 <option value="1">Grapefruit</option>
-        //                 <option value="lime">Lime</option>
-        //                 <option value="coconut">Coconut</option>
-        //                 <option value="mango">Mango</option>
-        //             </select>
-        //         </label>
-        //     </form>
-            <p>
-                {props.explanation}
-            </p>
-        // </>
+//         //             <select value={this.state.value} onChange={this.handleChange}>
+//         //                 {createOptionsWithNumbers}
+//         //             </select>
+//         //             <select value={this.state.value} onChange={this.handleChange}>
+//         //                 <option value="grapefruit">Grapefruit</option>
+//         //                 <option value="lime">Lime</option>
+//         //                 <option value="coconut">Coconut</option>
+//         //                 <option value="mango">Mango</option>
+//         //             </select>
+//         //             <select value={this.state.value} onChange={this.handleChange}>
+//         //                 <option value="1">Grapefruit</option>
+//         //                 <option value="lime">Lime</option>
+//         //                 <option value="coconut">Coconut</option>
+//         //                 <option value="mango">Mango</option>
+//         //             </select>
+//         //         </label>
+//         //     </form>
+//             <p>
+//                 {props.explanation}
+//             </p>
+//         // </>
         
-    )
-}
+//     )
+// }
 
-export default BottomContent;
+// export default SuperForm;
